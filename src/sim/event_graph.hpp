c2e2c098a6bbeda35fc2f6ea8332a3383560de35
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2023-02-01 19:16:42 +0200

    fix: make tree nodes referentially safe
    should look later into proper shared node ownership via shared pointers

--- src/sim/event_graph.hpp
+++ src/sim/event_graph.hpp
@@ -1,6 +1,6 @@
 #include <functional>
 #include <vector>
-
+#include <set>
 
 template<typename T>
 struct Operation {
@@ -11,12 +11,14 @@ template<typename T>
 class EventDAG {
     private:
     typename Operation<T>::fn operation;
-    std::vector<EventDAG<T>> followers;
+    std::vector<EventDAG<T>*> followers;
 
     public:
     EventDAG(typename Operation<T>::fn);
     void add_branch(typename Operation<T>::fn);
-    std::vector<EventDAG<T>> find_leaf_nodes();
+    void add_node(EventDAG<T>*);
+    bool is_leaf();
+    std::set<EventDAG<T>*> collect_leaf_nodes();
     std::vector<T> evaluate_depth(T);
     
     
@@ -27,27 +29,55 @@ template<typename T>
 EventDAG<T>::EventDAG(typename Operation<T>::fn op): operation(op) {};
 
 template<typename T> 
-void EventDAG<T>::add_branch(typename Operation<T>::fn op) 
-{
+void EventDAG<T>::add_branch(typename Operation<T>::fn op) {
     EventDAG next(op);
-    this->followers.push_back(next);
+    this->followers.push_back(&next);
 }
 
 template<typename T>
-std::vector<EventDAG<T>> EventDAG<T>::find_leaf_nodes() {
-    return this->followers;
+std::set<EventDAG<T>*> EventDAG<T>::collect_leaf_nodes() {
+    std::set<EventDAG<T>*> results;
+    
+    if(this->is_leaf()) {
+        results.insert(this);
+    }
+    else {
+        for(auto& n : this->followers) {
+            if(n->is_leaf()) {
+                results.insert(n);
+            }
+            else {
+                std::set<EventDAG<T>*> sub_results = n->collect_leaf_nodes();
+                for(auto& sub_result : sub_results) {
+                    results.insert(sub_result);
+                }
+            }
+        }
+    }
+    
+    return results;
+}
+
+template<typename T>
+bool EventDAG<T>::is_leaf() {
+    return this->followers.empty();
+}
+
+template<typename T>
+void EventDAG<T>::add_node(EventDAG<T>* next) {
+    this->followers.push_back(next);
 }
 
 template<typename T>
 std::vector<T> EventDAG<T>::evaluate_depth(T payload) {
     std::vector<T> results;
     T current = this->operation(payload);
-    if(this->followers.size() == 0) {
+    if(this->is_leaf()) {
         results.push_back(current);
     }
     else {
-        for(auto& n : this->followers) {
-            std::vector<T> sub_results = n.evaluate_depth(current);
+        for(auto n : this->followers) {
+            std::vector<T> sub_results = n->evaluate_depth(current);
             results.insert(results.end(), sub_results.begin(), sub_results.end());
         }        
     }
--- tests/event_graph.cpp
+++ tests/event_graph.cpp
@@ -10,14 +10,15 @@ int op(int a) {
 BOOST_AUTO_TEST_CASE(event_graph_initialization)
 {
     EventDAG<int> g(op);
-    std::vector<EventDAG<int>> leaves = g.find_leaf_nodes();
-    BOOST_CHECK(leaves.size() == 0);
+    std::set<EventDAG<int>*> leaves = g.collect_leaf_nodes();
+    BOOST_CHECK(leaves.size() == 1);
 };
 
 BOOST_AUTO_TEST_CASE(evalute_sequence)
 {
     EventDAG<int> root(op);
-    root.add_branch(op);
+    EventDAG<int> follower(op);
+    root.add_node(&follower);
     std::vector<int> depth_results = root.evaluate_depth(0);
     BOOST_CHECK(depth_results.size() == 1);
     BOOST_CHECK(depth_results.front() == 2);

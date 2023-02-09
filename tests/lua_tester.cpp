b11aa5613d91980975409cab60016ac271516e9b
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2023-02-09 13:42:42 +0200

    tests: add lua example for calling a host function

--- tests/lua_tester.cpp
+++ tests/lua_tester.cpp
@@ -5,11 +5,30 @@
 BOOST_AUTO_TEST_CASE(test_lua_function_call) {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
-    luaL_dofile(L, "test.lua");
-    lua_getglobal(L, "increment");
-    lua_pushinteger(L, 1);
-    lua_pcall(L, 1, 1, 0);
-    long result = lua_tointeger(L, -1);
+    luaL_dofile(L, "test.lua"); // execute given lua script file
+    lua_getglobal(L, "increment"); // get a function to call into the local lua stack
+    lua_pushinteger(L, 1); // push function argument to lua stack
+    lua_pcall(L, 1, 1, 0); // call lua "increment"
+    long result = lua_tointeger(L, -1); // value from top of stack as returned by lua "increment" function
     BOOST_CHECK(result == 2);
+    lua_close(L);
+}
+
+BOOST_AUTO_TEST_CASE(test_host_function_call) {
+    auto fn = [](lua_State* L) {
+        long num = lua_tointeger(L, 1);
+        long result = num + 1;
+        lua_pushinteger(L, result); // push to lua stack
+        return 1; // number of results
+    };
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+    lua_register(L, "host_fn", fn); // register local function into lua namespace
+    luaL_dofile(L, "test.lua");
+    lua_getglobal(L, "call_host_fn");
+    lua_pushinteger(L, 1); // push value to stack. will be the argument for "fn" closure
+    lua_pcall(L, 1, 1, 0); // call lua "call_host_fn", which will call our "fn" closure
+    long result = lua_tointeger(L, -1); // value from top of stack, as returned by "call_host_fn", pushed in "fn"
+    BOOST_CHECK(result == 2); // fn increments value by 1
     lua_close(L);
 }
--- tests/test.lua
+++ tests/test.lua
@@ -1,3 +1,7 @@
 function increment(num)
     return num + 1
 end
+
+function call_host_fn(num)
+    return host_fn(num)
+end

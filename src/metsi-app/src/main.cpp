2f73fc3308e0cbe326a32f2da6ce8ba940bd5278
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2023-03-10 14:39:30 +0200

    feat: flesh out initial executable
    features CLI positional arguments parsing, control file input and state file input

--- src/metsi-app/src/main.cpp
+++ src/metsi-app/src/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
-using namespace std;
+#include "app_io.hpp"
+#include "file_io.hpp"
 
-int main() {
-    cout<<"Hello world";
+int main(int argc, char** argv) {
+    auto args = positional_arguments(argc, argv);
+
+    std::cout << "Input data file: " << args["input-path"] << '\n';
+    std::cout << "Output data directory: " << args["output-path"] << '\n';
+    std::cout << "Control file: " << args["control-file"] << '\n';
+
+    YAML::Node control_data;
+    try {
+        control_data = read_yaml(args["control-file"]);
+    }
+    catch(std::exception&) {
+        std::cout << "Unable to read control file. Aborting.\n";
+        return 1;
+    }
+
+    std::vector<ForestStand> stands;
+    try {
+        stands = read_forest_csv(args["input-path"]);
+    }
+    catch(std::exception&) {
+        std::cout << "Unable to read input file. Aborting.\n";
+        return 1;
+    }
+
+    return 0;
 }

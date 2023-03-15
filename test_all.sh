ff0903272fd1c45e852e371ee477da8d32107c60
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2023-03-15 13:39:08 +0200

    chore: specify build presets for use with github actions
    Allows us to collect build environments under presets instead of custom calls

--- test_all.sh
+++ test_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-cmake . -B test_build
+cmake . -DCMAKE_TOOLCHAIN_FILE=./deps/vcpkg/scripts/buildsystems/vcpkg.cmake -B test_build
 cmake --build test_build
 ctest --test-dir test_build/src/metsi-app
 ctest --test-dir test_build/src/metsi-simlib

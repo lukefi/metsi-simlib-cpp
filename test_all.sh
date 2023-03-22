2406572fbe43db8b0dda0df591c47462e168ca98
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2023-03-22 14:19:13 +0200

    fix: refresh the local test wrapper!

--- test_all.sh
+++ test_all.sh
@@ -2,6 +2,6 @@
 set -e
 cmake . -DCMAKE_TOOLCHAIN_FILE=./deps/vcpkg/scripts/buildsystems/vcpkg.cmake -B test_build
 cmake --build test_build
-ctest --test-dir test_build/src/metsi-app
-ctest --test-dir test_build/src/metsi-simlib
+ctest --test-dir test_build/metsi-app
+ctest --test-dir test_build/metsi-simlib
 rm -rf test_build

b1f48921fde34d5efa008010df3a37706929c54b
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2023-04-03 13:05:17 +0300

    feat: a stand iterator for forest data csv
    Utilized the underlying CSVReader iterator to parse ForestStand objects.
    This implementation is minimally sufficient for using in range loops.
    This does not address proper traversal, or position awareness with iterators.
    The implementation is strictly iterable-once.

--- metsi-app/src/file_io.hpp
+++ metsi-app/src/file_io.hpp
@@ -1,10 +1,34 @@
 #ifndef FILE_IO_HPP
 #define FILE_IO_HPP
+#include <iterator>
 #include <yaml-cpp/yaml.h>
 #include "csv_parser.hpp"
 #include "state_model.hpp"
 
 YAML::Node read_yaml(const std::string&);
 std::vector<ForestStand> read_forest_csv(const std::string&);
+
+class ForestStandCSVReader {
+    csv::CSVReader* reader;
+public:
+    class iterator {
+        csv::CSVReader::iterator csv_iter;
+        csv::CSVReader* reader;
+
+        ForestStand parse_stand();
+    public:
+        struct sentinel{ static bool done(const iterator& it) { return it.csv_iter == it.reader->end(); }; };
+        explicit iterator(csv::CSVReader*);
+        ForestStand operator*() { return parse_stand(); };
+        iterator operator++() { return *this; };
+        friend bool operator==(const iterator& a, sentinel) { return ForestStandCSVReader::iterator::sentinel::done(a); };
+        friend bool operator!=(const iterator& a, sentinel) { return !ForestStandCSVReader::iterator::sentinel::done(a); };
+    };
+
+public:
+    explicit ForestStandCSVReader(const std::string&);
+    [[nodiscard]] auto begin() const { return iterator(reader); }
+    [[nodiscard]] static auto end() { return iterator::sentinel{}; }
+};
 
 #endif
--- metsi-app/tests/file_io_test.cpp
+++ metsi-app/tests/file_io_test.cpp
@@ -17,3 +17,18 @@ BOOST_AUTO_TEST_CASE(read_forest_stands) {
     BOOST_CHECK(result[0].trees[0].get<int>("species") == 2);
     BOOST_CHECK_CLOSE(result[0].trees[0].get<float>("stems_ha"), 1.78f, 0.1f);
 }
+
+BOOST_AUTO_TEST_CASE(forest_stand_iterator) {
+    auto fs_iter = ForestStandCSVReader("test_resources/forest_data_multiple.csv");
+    int counter = 0;
+    std::vector<ForestStand> results;
+    for(auto stand : fs_iter) {
+        counter++;
+        results.emplace_back(stand);
+    }
+    BOOST_CHECK_EQUAL(counter, 2);
+    BOOST_CHECK_EQUAL(results[0].get<std::string>("identifier"), "43174747");
+    BOOST_CHECK_EQUAL(results[0].trees.size(), 9);
+    BOOST_CHECK_EQUAL(results[1].get<std::string>("identifier"), "43174748");
+    BOOST_CHECK_EQUAL(results[1].trees.size(), 9);
+}
